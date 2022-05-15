fe9f3be7be8c753ebe7332dfbccd7debe106fceb
Christian Tonti <ctonti@umd.edu>
2022-05-15 18:04:31 -0400

    Update config

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,3 +1,5 @@
+import os
+
 # Stores all configuration values
 SECRET_KEY = b'{\xba\x04\xdb\x8c\n\x17\xae#J\xd6\xbb\xa4\x1c:\xc1'
-MONGODB_HOST = "mongodb://localhost:27017/388J_Final"
+MONGODB_HOST = os.getenv('MONGODB_HOST')
